e56a50ca2649be9828f03fddd0feb5bbaa105b87
houzhangbo <1532558305@qq.com>
2015-12-26 14:41:43 +0800

    add

--- src/main/java/com/my/spring/ctrl/TestCtrl.java
+++ src/main/java/com/my/spring/ctrl/TestCtrl.java
@@ -5,13 +5,14 @@ import java.util.*;
 import javax.servlet.http.*;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.my.spring.form.UserInfo;
-
+//http://www.blogjava.net/soken/articles/371548.html  ²Î¿¼´ËÎÄÕÂ
 @Controller
 @RequestMapping("/userinfo")
 public class TestCtrl {
@@ -20,6 +21,20 @@ public class TestCtrl {
 	public ModelAndView _new(HttpServletRequest request,HttpServletResponse response,UserInfo userInfo)
 			throws Exception{
 		return new ModelAndView("/userinfo/new","userInfo",userInfo);
+	}
+	
+	@RequestMapping(value="/getStr")
+	public String getStr(HttpServletRequest request,HttpServletResponse response,UserInfo userInfo)
+			throws Exception{
+		request.setAttribute("test", userInfo.getName());
+		return "str";
+	}
+	
+	@RequestMapping(value="/{id}/getById")
+	public String getById(@PathVariable Long id,HttpServletRequest request,HttpServletResponse response,UserInfo userInfo)
+			throws Exception{
+		request.setAttribute("test", id);
+		return "str";
 	}
 	
 	@RequestMapping(value="/test")
---
+++ src/main/webapp/str.jsp
@@ -0,0 +1,6 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<body>
+${test}
+</body>
+</html>
